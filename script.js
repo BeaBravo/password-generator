6b21f108792e17634a7e9ca1da5e97f3d5546889
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-23 22:01:03 -0700

    added prompts for user input

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  var password = "Hello this is a password";
   var passwordText = document.querySelector("#password");
+  var passwordLength = Number(
+    prompt("Choose a password length between 8 and 128 characters")
+  );
+  var isUpperCase = confirm("include uppercase characters?");
+  var isNumber = confirm("include numbers?");
+  var specialChar = confirm("include special characters?");
+
+  console.log(passwordLength);
+  console.log(isUpperCase);
+  console.log(isNumber);
+  console.log(specialChar);
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
