e3be3a36eca3014d09a66c9b6b80ddbebfa73e78
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-23 22:43:08 -0700

    check which conditions will be used for password

--- script.js
+++ script.js
@@ -9,14 +9,55 @@ function writePassword() {
   var passwordLength = Number(
     prompt("Choose a password length between 8 and 128 characters")
   );
+  var isLowerCase = confirm("include lowercase characters?");
   var isUpperCase = confirm("include uppercase characters?");
   var isNumber = confirm("include numbers?");
   var specialChar = confirm("include special characters?");
 
-  console.log(passwordLength);
-  console.log(isUpperCase);
-  console.log(isNumber);
-  console.log(specialChar);
+  // check if all conditions are true
+  if (isLowerCase && isUpperCase && isNumber && specialChar) {
+    console.log("we're using them all");
+  } // check if one of them is false
+  else if (!isLowerCase && isUpperCase && isNumber && specialChar) {
+    console.log("don't use lowercase characters!");
+  } else if (isLowerCase && !isUpperCase && isNumber && specialChar) {
+    console.log("don't use uppercase characters!");
+  } else if (isLowerCase && isUpperCase && !isNumber && specialChar) {
+    console.log("don't use numeric characters!");
+  } else if (isLowerCase && isUpperCase && isNumber && !specialChar) {
+    console.log("don't use special characters!");
+  } //check if two conditions are false
+  else if (!isLowerCase && !isUpperCase && isNumber && specialChare) {
+    console.log("don't use any letters");
+  } else if (!isLowerCase && isUpperCase && !isNumber && specialChar) {
+    console.log("don't use lowercase or numbers!");
+  } else if (!isLowerCase && isUpperCase && isNumber && !specialChar) {
+    console.log("don't use lowercase letters or special characters");
+  } else if (isLowerCase && !isUpperCase && !isNumber && specialChar) {
+    console.log("don't use uppercase or numbers");
+  } else if (isLowerCase && !isUpperCase && isNumber && !specialChar) {
+    console.log("don't use uppercase or special characters");
+  } else if (isLowerCase && isUpperCase && !isNumber && !specialChar) {
+    console.log("don't use numbers or special characters");
+  } //check if only is true
+  else if (isLowerCase && !isUpperCase && !isNumber && !specialChar) {
+    console.log("only use lowercase characters");
+  } else if (!isLowerCase && isUpperCase && !isNumber && !specialChar) {
+    console.log("only use uppercase characters");
+  } else if (!isLowerCase && !isUpperCase && isNumber && !specialChar) {
+    console.log("only use numeric characters");
+  } else if (!isLowerCase && !isUpperCase && !isNumber && specialChar) {
+    console.log("only use special characters");
+  } //if they're all false
+  else {
+    console.log("one of the requirements must be selected");
+  }
+
+  console.log("password length", passwordLength);
+  console.log("lowercase", isLowerCase);
+  console.log("uppercase", isUpperCase);
+  console.log("numbers", isNumber);
+  console.log("special characters", specialChar);
 
   passwordText.value = password;
 }
