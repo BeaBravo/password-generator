e21b6b627682b7c6d517cda851c10f3504e95f79
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-25 16:46:46 -0700

    added randon index selection

--- script.js
+++ script.js
@@ -21,83 +21,185 @@ function writePassword() {
   var isUpperCase = confirm("include uppercase characters?");
   var isNumber = confirm("include numbers?");
   var specialChar = confirm("include special characters?");
-  var charactersLowercase = ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"];
-  var charactersUppercase = ["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"];
-  var charactersNumbers = ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"];
-  var charactersSpecial = ["!" "#" "$" "%" "&" "'" "(" ")" "*" "+" "," "-" "." "/" ":" ";" "<" "=" ">" "?" "@" "[" "]" "^" "_" "`" "{" "|" "}" "~" '"'];
+  var charactersLowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var charactersUppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var charactersNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var charactersSpecial = [
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+    '"',
+  ];
+  var characters = [];
 
   // check if all conditions are true and asign which characters to use
   if (isLowerCase && isUpperCase && isNumber && specialChar) {
     console.log("we're using them all");
-    var characters =
-      charactersLowercase +
-      charactersUppercase +
-      charactersSpecial +
-      charactersNumbers;
+    characters = characters.concat(
+      charactersLowercase,
+      charactersUppercase,
+      charactersNumbers,
+      charactersSpecial
+    );
   } // check if one of them is false
   else if (!isLowerCase && isUpperCase && isNumber && specialChar) {
     console.log("don't use lowercase characters!");
-    var characters =
-      charactersUppercase + charactersSpecial + charactersNumbers;
+    characters = characters.concat(
+      charactersUppercase,
+      charactersSpecial,
+      charactersNumbers
+    );
   } else if (isLowerCase && !isUpperCase && isNumber && specialChar) {
     console.log("don't use uppercase characters!");
-    var characters =
-      charactersLowercase + charactersSpecial + charactersNumbers;
+    characters = characters.concat(
+      charactersLowercase,
+      charactersSpecial,
+      charactersNumbers
+    );
   } else if (isLowerCase && isUpperCase && !isNumber && specialChar) {
     console.log("don't use numeric characters!");
-    var characters =
-      charactersLowercase + charactersUppercase + charactersSpecial;
+    characters = characters.concat(
+      charactersLowercase,
+      charactersUppercase,
+      charactersSpecial
+    );
   } else if (isLowerCase && isUpperCase && isNumber && !specialChar) {
     console.log("don't use special characters!");
-    var characters =
-      charactersLowercase + charactersUppercase + charactersNumbers;
+    characters = characters.concat(
+      charactersLowercase,
+      charactersUppercase,
+      charactersNumbers
+    );
   } //check if two conditions are false
   else if (!isLowerCase && !isUpperCase && isNumber && specialChare) {
     console.log("don't use any letters");
-    var characters = charactersSpecial + charactersNumbers;
+    characters = characters.concat(charactersSpecial, charactersNumbers);
   } else if (!isLowerCase && isUpperCase && !isNumber && specialChar) {
     console.log("don't use lowercase or numbers!");
-    var characters = charactersUppercase + charactersSpecial;
+    characters = characters.concat(charactersUppercase, charactersSpecial);
   } else if (!isLowerCase && isUpperCase && isNumber && !specialChar) {
     console.log("don't use lowercase letters or special characters");
-    var characters = charactersUppercase + charactersNumbers;
+    characters = characters.concat(charactersUppercase, charactersNumbers);
   } else if (isLowerCase && !isUpperCase && !isNumber && specialChar) {
     console.log("don't use uppercase or numbers");
-    var characters = charactersLowercase + charactersSpecial;
+    characters = characters.concat(charactersLowercase, charactersSpecial);
   } else if (isLowerCase && !isUpperCase && isNumber && !specialChar) {
     console.log("don't use uppercase or special characters");
-    var characters = charactersLowercase + charactersNumbers;
+    characters = characters.concat(charactersLowercase, charactersNumbers);
   } else if (isLowerCase && isUpperCase && !isNumber && !specialChar) {
     console.log("don't use numbers or special characters");
-    var characters = charactersLowercase + charactersUppercase;
+    characters = characters.concat(charactersLowercase, charactersUppercase);
   } //check if only is true
   else if (isLowerCase && !isUpperCase && !isNumber && !specialChar) {
     console.log("only use lowercase characters");
-    var characters = charactersLowercase;
+    characters = charactersLowercase;
   } else if (!isLowerCase && isUpperCase && !isNumber && !specialChar) {
     console.log("only use uppercase characters");
-    var characters = charactersUppercase;
+    characters = charactersUppercase;
   } else if (!isLowerCase && !isUpperCase && isNumber && !specialChar) {
     console.log("only use numeric characters");
-    var characters = charactersNumbers;
+    characters = charactersNumbers;
   } else if (!isLowerCase && !isUpperCase && !isNumber && specialChar) {
     console.log("only use special characters");
-    var characters = charactersSpecial;
+    characters = charactersSpecial;
   } //if they're all false
   else {
     console.log("one of the requirements must be selected");
   }
 
-  // create random password with the characters and length selected 
-  password = characters;
+  // create random password with the characters and length selected
+  var index = Math.floor(Math.random() * characters.length);
+  password = characters[index];
 
-  //console.log just for debugging 
+  //console.log just for debugging
   console.log("password length", passwordLength);
   console.log("lowercase", isLowerCase);
   console.log("uppercase", isUpperCase);
   console.log("numbers", isNumber);
   console.log("special characters", specialChar);
   console.log("characters", characters);
+  console.log("index", index);
   // }
   passwordText.value = password;
 }
